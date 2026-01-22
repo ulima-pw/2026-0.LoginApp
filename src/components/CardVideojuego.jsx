18bfa0306e7e3d79f9cb9c41f8818f6483eea7aa
ProfL3301 <ProfL3301@lab.aca>
2026-01-22 15:53:17 -0500

    CardVideojuegos implementado

---
+++ src/components/CardVideojuego.jsx
@@ -0,0 +1,14 @@
+function CardVideojuego({ videojuego }) {
+    return <div key={videojuego.nombre} className="border rounded-md p-4">
+        <h2 className="text-2xl mb-2 text-orange-700">{videojuego.nombre}</h2>
+        <img className="mb-2" src={videojuego.imagen} />
+        <p className="mb-2 text-sm">
+            {videojuego.descripcion}
+        </p>
+        <span className="text-xs bg-orange-600 rounded-full text-white px-2 py-1">
+            {videojuego.categoria}
+        </span>
+    </div>
+}
+
+export default CardVideojuego
--- src/components/GrillaVideojuegos.jsx
+++ src/components/GrillaVideojuegos.jsx
@@ -1,60 +1,29 @@
+import CardVideojuego from "./CardVideojuego"
+
 function GrillaVideojuegos() {
+    const listaVideojuegos = [
+        {
+            nombre: "CSGO",
+            imagen : "/imagenes/csgo.jpg",
+            descripcion : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor eros sed semper aliquam. Duis quis tellus libero. Quisque consequat mauris eu molestie vestibulum. Nunc at ligula aliquet, accumsan dui ac, lacinia nulla. Quisque in aliquam nisl. Maecenas faucibus est orci, non facilisis arcu vestibulum ut. Nulla congue dapibus sagittis. Pellentesque et lectus at ante convallis tristique vitae in dolor. Donec varius mauris id enim cursus ultrices. Sed a lobortis quam.",
+            categoria : "FPS"
+        },{
+            nombre: "GTA6",
+            imagen : "https://www.pngall.com/wp-content/uploads/15/Grand-Theft-Auto-VI-Logo-PNG-Images.png",
+            descripcion : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+            categoria : "OpenWorld"
+        }
+    ]
+
+
     return <div className="grid grid-cols-3 gap-2">
-        <div className="border rounded-md p-4">
-            <h2 className="text-2xl mb-2 text-orange-700">CSGO</h2>
-            <img className="mb-2"
-                src="/imagenes/csgo.jpg" />
-            <p className="mb-2 text-sm">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor eros sed semper aliquam. Duis quis tellus libero. Quisque consequat mauris eu molestie vestibulum. Nunc at ligula aliquet, accumsan dui ac, lacinia nulla. Quisque in aliquam nisl. Maecenas faucibus est orci, non facilisis arcu vestibulum ut. Nulla congue dapibus sagittis. Pellentesque et lectus at ante convallis tristique vitae in dolor. Donec varius mauris id enim cursus ultrices. Sed a lobortis quam.
-            </p>
-            <span className="text-xs bg-orange-600 rounded-full text-white px-2 py-1">
-                RPG
-            </span>
-        </div>
-        <div className="border rounded-md p-4">
-            <h2 className="text-2xl mb-2 text-orange-700">CSGO</h2>
-            <img className="mb-2"
-                src="/imagenes/csgo.jpg" />
-            <p className="mb-2 text-sm">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor eros sed semper aliquam. Duis quis tellus libero. Quisque consequat mauris eu molestie vestibulum. Nunc at ligula aliquet, accumsan dui ac, lacinia nulla. Quisque in aliquam nisl. Maecenas faucibus est orci, non facilisis arcu vestibulum ut. Nulla congue dapibus sagittis. Pellentesque et lectus at ante convallis tristique vitae in dolor. Donec varius mauris id enim cursus ultrices. Sed a lobortis quam.
-            </p>
-            <span className="text-xs bg-orange-600 rounded-full text-white px-2 py-1">
-                RPG
-            </span>
-        </div>
-        <div className="border rounded-md p-4">
-            <h2 className="text-2xl mb-2 text-orange-700">CSGO</h2>
-            <img className="mb-2"
-                src="/imagenes/csgo.jpg" />
-            <p className="mb-2 text-sm">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor eros sed semper aliquam. Duis quis tellus libero. Quisque consequat mauris eu molestie vestibulum. Nunc at ligula aliquet, accumsan dui ac, lacinia nulla. Quisque in aliquam nisl. Maecenas faucibus est orci, non facilisis arcu vestibulum ut. Nulla congue dapibus sagittis. Pellentesque et lectus at ante convallis tristique vitae in dolor. Donec varius mauris id enim cursus ultrices. Sed a lobortis quam.
-            </p>
-            <span className="text-xs bg-orange-600 rounded-full text-white px-2 py-1">
-                RPG
-            </span>
-        </div>
-        <div className="border rounded-md p-4">
-            <h2 className="text-2xl mb-2 text-orange-700">CSGO</h2>
-            <img className="mb-2"
-                src="/imagenes/csgo.jpg" />
-            <p className="mb-2 text-sm">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor eros sed semper aliquam. Duis quis tellus libero. Quisque consequat mauris eu molestie vestibulum. Nunc at ligula aliquet, accumsan dui ac, lacinia nulla. Quisque in aliquam nisl. Maecenas faucibus est orci, non facilisis arcu vestibulum ut. Nulla congue dapibus sagittis. Pellentesque et lectus at ante convallis tristique vitae in dolor. Donec varius mauris id enim cursus ultrices. Sed a lobortis quam.
-            </p>
-            <span className="text-xs bg-orange-600 rounded-full text-white px-2 py-1">
-                RPG
-            </span>
-        </div>
-        <div className="border rounded-md p-4">
-            <h2 className="text-2xl mb-2 text-orange-700">CSGO</h2>
-            <img className="mb-2"
-                src="/imagenes/csgo.jpg" />
-            <p className="mb-2 text-sm">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor eros sed semper aliquam. Duis quis tellus libero. Quisque consequat mauris eu molestie vestibulum. Nunc at ligula aliquet, accumsan dui ac, lacinia nulla. Quisque in aliquam nisl. Maecenas faucibus est orci, non facilisis arcu vestibulum ut. Nulla congue dapibus sagittis. Pellentesque et lectus at ante convallis tristique vitae in dolor. Donec varius mauris id enim cursus ultrices. Sed a lobortis quam.
-            </p>
-            <span className="text-xs bg-orange-600 rounded-full text-white px-2 py-1">
-                RPG
-            </span>
-        </div>
+        {
+            listaVideojuegos.map(function(videojuego) {
+                return <CardVideojuego 
+                    key={videojuego.nombre} 
+                    videojuego={videojuego} />
+            })
+        }
     </div>
 }
 
