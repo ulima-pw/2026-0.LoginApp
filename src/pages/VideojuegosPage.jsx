aa812eba324f8b466a013519594af729290f7ad9
ProfL3301 <ProfL3301@lab.aca>
2026-01-22 16:47:52 -0500

    Ejemplo terminado

--- src/pages/VideojuegosPage.jsx
+++ src/pages/VideojuegosPage.jsx
@@ -4,18 +4,18 @@ import GrillaVideojuegos from "../components/GrillaVideojuegos"
 import Titulo from "../components/Titulo"
 
 const lista = [
-        {
-            nombre: "CSGO",
-            imagen : "/imagenes/csgo.jpg",
-            descripcion : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor eros sed semper aliquam. Duis quis tellus libero. Quisque consequat mauris eu molestie vestibulum. Nunc at ligula aliquet, accumsan dui ac, lacinia nulla. Quisque in aliquam nisl. Maecenas faucibus est orci, non facilisis arcu vestibulum ut. Nulla congue dapibus sagittis. Pellentesque et lectus at ante convallis tristique vitae in dolor. Donec varius mauris id enim cursus ultrices. Sed a lobortis quam.",
-            categoria : "FPS"
-        },{
-            nombre: "GTA6",
-            imagen : "https://www.pngall.com/wp-content/uploads/15/Grand-Theft-Auto-VI-Logo-PNG-Images.png",
-            descripcion : "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
-            categoria : "OpenWorld"
-        }
-    ]
+    {
+        nombre: "CSGO",
+        imagen: "/imagenes/csgo.jpg",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor eros sed semper aliquam. Duis quis tellus libero. Quisque consequat mauris eu molestie vestibulum. Nunc at ligula aliquet, accumsan dui ac, lacinia nulla. Quisque in aliquam nisl. Maecenas faucibus est orci, non facilisis arcu vestibulum ut. Nulla congue dapibus sagittis. Pellentesque et lectus at ante convallis tristique vitae in dolor. Donec varius mauris id enim cursus ultrices. Sed a lobortis quam.",
+        categoria: "FPS"
+    }, {
+        nombre: "GTA6",
+        imagen: "https://www.pngall.com/wp-content/uploads/15/Grand-Theft-Auto-VI-Logo-PNG-Images.png",
+        descripcion: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. ",
+        categoria: "OpenWorld"
+    }
+]
 
 function VideojuegosPage() {
     const categorias = [
@@ -27,7 +27,7 @@ function VideojuegosPage() {
         if (categoria == "-1") {
             setListaVideojuegos(lista)
         }else {
-            const listaVideojuegosModificado = listaVideojuegos.filter( function(vj) {
+            const listaVideojuegosModificado = lista.filter( function(vj) {
                 return vj.categoria == categoria
             } )
             setListaVideojuegos(listaVideojuegosModificado)
